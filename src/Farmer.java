4d029647f2d29dda47ce651ca39f0bbcf129baf1
kong_user <kong_uesr@gmail.com>
2018-12-30 19:09:16 +0800

    修改后的第三次提交farmer

--- src/Farmer.java
+++ src/Farmer.java
@@ -4,6 +4,8 @@ public class Farmer {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("农民信息");
+		System.out.println("有效信息");
+		System.out.println("进展与调度");
 	}
 
 }
