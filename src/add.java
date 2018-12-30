b59159210c2f1f422b89a3101d5089d4dc8e9048
peter_glb <peter_glb@gmail.com>
2018-12-30 22:22:16 +0800

    kong2天猫国际

---
+++ src/add.java
@@ -0,0 +1,11 @@
+public class Farmer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("xxxx");
+		System.out.println("yyyy");
+		System.out.println("天猫国际");
+		System.out.println("精品路线");
+	}
+
+}
