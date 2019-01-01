1edad3e1f1de9e6dc2de884b5bc85a8ab0ee0875
kong_user <kong_uesr@gmail.com>
2019-01-01 20:20:07 +0800

    k教师类last

--- src/Teacher.java
+++ src/Teacher.java
@@ -6,8 +6,6 @@ public class Teacher {
 		System.out.println("为人师表");
 		System.out.println("学高为师");
 		System.out.println("行为世范");
-		System.out.println("教师学期考核");
-		System.out.println("k2新分支的修改");
 	}
 
 }
